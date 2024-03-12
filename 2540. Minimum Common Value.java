227fa87014397e33e113cb64abce80cb80d30406
Rut99 <57997911+Rut99@users.noreply.github.com>
2024-03-13 01:03:21 +0530

    Create 2540. Minimum Common Value.java

---
+++ 2540. Minimum Common Value.java
@@ -0,0 +1,29 @@
+/*
+2540. Minimum Common Value
+Example 1:
+Input: nums1 = [1,2,3], nums2 = [2,4]
+Output: 2
+Explanation: The smallest element common to both arrays is 2, so we return 2.
+Example 2:
+Input: nums1 = [1,2,3,6], nums2 = [2,3,4,5]
+Output: 2
+Explanation: There are two common elements in the array 2 and 3 out of which 2 is the smallest, so 2 is returned.
+*/
+
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        int min = -1;
+        int i = 0;
+        int j = 0;
+        while(i < nums1.length && j < nums2.length){
+            if(nums1[i] < nums2[j]){
+                i++;
+            }else if(nums1[i] > nums2[j]){
+                j++;
+            }else{
+                return nums1[i];
+            }
+        }
+        return -1;
+    }
+}
